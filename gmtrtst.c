a656af418a54f0b365a4e24129c525e1f0da8938
Koshetaur <60685097+Koshetaur@users.noreply.github.com>
2020-03-09 19:51:09 +0700

    write testcircle and finish data testing programm

--- gmtrtst.c
+++ gmtrtst.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+int poww(int x, int y){
+	int i=0, x1=1;
+	for(i=0;i<y;i++){
+		x1*=x;
+	}
+	return x1;
+}
 int testshape(char fig[25]){
 	int c=0, i=0;
 	char f1[]="circle";
@@ -25,14 +32,43 @@ int testformat(char fig[25]){
 		printf("Error: invalid format\n");
 	return 0;
 }
+int testcircle(char fig[25]){
+	int i=0, c=0, f;
+	float r=0;
+	while(fig[i]!=')'){
+		i++;}
+	while(fig[i-1]!='.'){
+		i--;
+		if(fig[i]==' '){
+			printf("Error: invalid format\n");
+			break;
+		}
+		else{
+			f=atoi(&fig[i]);
+			r=(r/10)+f;
+			f=0;
+		}
+	}
+	i-=2; r/=10; f=0;
+	while(fig[i]!=' '){
+		f=atoi(&fig[i]);
+		r+=(f*poww(10,c));
+		c++; i--; f=0;
+	}
+	if (r<=0)
+		printf("Error: invalid circle\n");
+	return 0;
+}
 int main(){
 	int i=0;
 	char fig[25];
 	printf("write your circle in format 'circle(x y, r)', r with .0 if it is integer\n");
+	//circle(0 0, 1.5)
 	while(fig[i-1]!=')'){
 		scanf("%c", &fig[i]); i++;
 	}
 	testshape(fig);
 	testformat(fig);
+	testcircle(fig);
 	return 0;
 }
