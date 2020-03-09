8565cb566d4db7cf208576b51aaf5a19280ce6c6
Koshetaur <60685097+Koshetaur@users.noreply.github.com>
2020-03-09 17:32:29 +0700

    write testhape function

--- gmtrtst.c
+++ gmtrtst.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+int testshape(char fig[25]){
+	int c=0, i=0;
+	char f1[]="circle";
+	for(i=0;i<6;i++){
+		if(fig[i]==f1[i])
+			c++;
+		}
+	i=0;
+	if(c!=6)
+		printf("Error: unknown shape\n");
+	return 0;
+}
 int main(){
 	int i=0;
 	char fig[25];
@@ -7,5 +19,6 @@ int main(){
 	while(fig[i-1]!=')'){
 		scanf("%c", &fig[i]); i++;
 	}
+	testshape(fig);
 	return 0;
 }
