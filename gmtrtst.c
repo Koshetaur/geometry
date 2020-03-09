02eaaba088e2b5c2f30f65ed479af79b24a1e547
Koshetaur <60685097+Koshetaur@users.noreply.github.com>
2020-03-09 18:42:53 +0700

    write testformat function

--- gmtrtst.c
+++ gmtrtst.c
@@ -12,6 +12,19 @@ int testshape(char fig[25]){
 		printf("Error: unknown shape\n");
 	return 0;
 }
+int testformat(char fig[25]){
+	int p=0, c=0, i=0;
+	while(fig[i]!=')'){
+		if(fig[i]=='1' || fig[i]=='2' || fig[i]=='3' || fig[i]=='4' || fig[i]=='5' || fig[i]=='6' || fig[i]=='7' || fig[i]=='8' || fig[i]=='9' || fig[i]=='0')
+			{c++;}
+		if(fig[i]==',' || fig[i]==' ')
+			{p++;}
+		i++;
+	}
+	if(p!=3 || c<3)
+		printf("Error: invalid format\n");
+	return 0;
+}
 int main(){
 	int i=0;
 	char fig[25];
@@ -20,5 +33,6 @@ int main(){
 		scanf("%c", &fig[i]); i++;
 	}
 	testshape(fig);
+	testformat(fig);
 	return 0;
 }
